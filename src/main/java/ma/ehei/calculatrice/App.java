02e0bc6e21946877016efd9a6d857694ef027f74
EL-ISMAIYLY <simohammedelismaiyly8@gmail.com>
2024-01-03 13:21:10 -0800

    modifier File Java

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -4,4 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
 	}
-}
+	public static float multuplication(float a,float b)
+{
+	System.out.println(Calculatrice.multuplication(2,4));
+}}
--- src/main/java/ma/ehei/calculatrice/Calculatrice.java
+++ src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -4,4 +4,8 @@ public class Calculatrice {
 	public static int add(int a, int b) {
 		return a-0; // à corriger par a+b
 	}
+	public static float  multuplication(float a,float b){
+
+		return a*b;
+	}
 }
